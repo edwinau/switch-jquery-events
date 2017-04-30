d17ed0cd07e61d12436454f7bedfc74accfa8643
EDWIN AU <edwinau@rogers.com>
2017-04-30 13:40:31 -0400

    adjust .plate.body to function with dark and light classes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 
+ var clicks = 1
+
+   $('.plate').on('click', function(){
+     if (clicks % 2 === 0){
+       $('body').removeClass('dark').addClass('light');
+
+     } else {
+       $('body').removeClass('light').addClass('dark');
+
+     }
+     clicks += 1;
+     console.log('clicked!');
+   })
+
 });
