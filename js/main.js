6067be8bdc97bef1ac8abcf1f7e33ed791111572
EDWIN AU <edwinau@rogers.com>
2017-04-30 13:46:51 -0400

    adjust .status to read It IS DARK when lights are off.

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
      if (clicks % 2 === 0){
        $('body').removeClass('dark').addClass('light');
        $('button').removeClass('off').addClass('on');
-
+       $('.status').text("It's so Bright");
      } else {
        $('body').removeClass('light').addClass('dark');
        $('button').removeClass('on').addClass('off');
-
+       $('.status').text("Who turned off the lights?");
      }
      clicks += 1;
      console.log('clicked!');
